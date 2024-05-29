c04ffa00f4346e6c9ad04c4b30dee52307a38b3c
MD JAMIL AKHTAR <131196293+mdjamilakhtar793@users.noreply.github.com>
2024-05-29 15:36:17 +0530

    AdminSidebar changes

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -59,7 +59,7 @@ const AdminSidebar = () => {
             : {}
         }
       >
-        <h2>Logo.</h2>
+        <img src="/TC.png" alt="photo" sizes="auto" />
         <DivOne location={location} />
         <DivTwo location={location} />
         <DivThree location={location} />
